9abf115b3198c6431c926defc8b0fc301dfa3d97
Reid-T-W <rediettadesse100@gmail.com>
2022-08-23 04:04:42 +0300

    defination of function infinite_while removed

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,18 +2,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 /**
- *infinite_while - infinite_while loop to keep the parent process running
- *Return: int
- */
-int infinite_while(void)
-{
-	while (1)
-	{
-		sleep(1);
-	}
-	return (0);
-}
-/**
  * main - main function that creates 5 zombie processes
  * Return: void
  */
