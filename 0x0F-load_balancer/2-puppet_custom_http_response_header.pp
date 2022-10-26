e3303216721e71b9802804d97ac48057e1672fb2
Reid-T-W <rediettadesse100@gmail.com>
2022-10-26 06:07:44 +0300

    manual installation

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # (puppet manifest that configures a brand new server and sets custom response header)
-package {'nginx':
-  ensure => 'installed',
+exec {'command_install':
+  command => 'apt-get -y update;apt-get -y install nginx;ufw allow \'Nginx HTTP\''
 }
 exec {'command_header':
   notify  => Service['nginx'],
