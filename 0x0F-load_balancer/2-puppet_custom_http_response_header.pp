6e9d3d513872842fd061be99cc71f102ac2e61d1
Reid-T-W <rediettadesse100@gmail.com>
2022-10-26 06:22:16 +0300

    provider added

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,14 @@
 # (puppet manifest that configures a brand new server and sets custom response header)
 exec {'command_install':
-  command => 'apt-get -y update;apt-get -y install nginx',
-  path    => '/usr/bin',
+  command  => 'apt-get -y update;apt-get -y install nginx',
+  path     => '/usr/bin',
+  provider => shell,
 }
 exec {'command_header':
   notify  => Service['nginx'],
   path    => '/bin',
   command => "sed -i \"42i\\	add_header X-Served-By ${hostname};\" /etc/nginx/sites-available/default",
+  provider => shell,
 }
 service {'nginx':
   ensure => 'running',
