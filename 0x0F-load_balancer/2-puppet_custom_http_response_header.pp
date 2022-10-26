f058908e4a6d3df2f57e18eeb431c71b5ec3e375
Reid-T-W <rediettadesse100@gmail.com>
2022-10-26 05:58:07 +0300

    only header modification

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,27 +2,6 @@
 package {'nginx':
   ensure => 'installed',
 }
-file {'/var/www/html/index.nginx-debian.html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World!',
-}
-file {'/usr/share/nginx/html/custom_404':
-  ensure  => 'present',
-  path    => '/usr/share/nginx/html/custom_404',
-  content => 'Ceci n\'est pas une page',
-}
-exec {'command_301':
-  notify  => Service['nginx'],
-  path    => '/bin',
-  command => 'sed -i "49i\	location \/redirect_me {\n		try_files \$uri =301;\n	}" /etc/nginx/sites-available/default',
-}
-exec {'command_404':
-  notify  => Service['nginx'],
-  path    => '/bin',
-  command => 'sed -i "49i\	error_page 404 \/custom_404;\n	location = \/custom_404 \
-{\n		root \/usr\/share\/nginx\/html;\n		internal;\n	}" /etc/nginx/sites-available/default',
-}
 exec {'command_header':
   notify  => Service['nginx'],
   path    => '/bin',
