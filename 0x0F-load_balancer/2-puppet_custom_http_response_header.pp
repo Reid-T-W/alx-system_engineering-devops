d87face15b49b6b60206ce12f2c396da9bb55ce3
Reid-T-W <rediettadesse100@gmail.com>
2022-10-26 05:18:19 +0300

    cutom 301 removed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,20 +2,6 @@
 package {'nginx':
   ensure => 'installed',
 }
-file {'/var/www/html/index.nginx-debian.html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World!',
-}
-file {'/usr/share/nginx/html/custom_404':
-  ensure  => 'present',
-  path    => '/usr/share/nginx/html/custom_404',
-  content => 'Ceci n\'est pas une page',
-}
-exec {'command_301':
-  path    => '/bin',
-  command => 'sed -i "49i\	location \/redirect_me {\n		try_files \$uri =301;\n	}" /etc/nginx/sites-available/default',
-}
 exec {'command_header':
   path    => '/bin',
   command => "sed -i \"42i\	add_header X-Served-By ${hostname};\" /etc/nginx/sites-available/default",
