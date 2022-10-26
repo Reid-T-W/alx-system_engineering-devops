8644e473b65558fe46577b98cfbe175d0b785fa3
Reid-T-W <rediettadesse100@gmail.com>
2022-10-26 04:47:22 +0300

    Configures a new web server using puppet and adds custom response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,31 @@
+# (puppet manifest that configures a brand new server and sets custom response header)
+package {'nginx':
+  ensure => 'installed',
+}
+file {'/var/www/html/index.nginx-debian.html':
+  ensure  => 'present',
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+}
+file {'/usr/share/nginx/html/custom_404':
+  ensure  => 'present',
+  path    => '/usr/share/nginx/html/custom_404',
+  content => 'Ceci n\'est pas une page',
+}
+exec {'command_301':
+  path    => '/bin',
+  command => 'sed -i "48i\	location \/redirect_me {\n		try_files \$uri =301;\n	}" test_default',
+}
+exec {'command_404':
+  path    => '/bin',
+  command => 'sed -i "48i\	error_page 404 \/custom_404;\n	location = \/custom_404 \
+{\n		root \/usr\/share\/nginx\/html;\n		internal;\n	}" test_default',
+}
+exec {'command_header':
+  path    => '/bin',
+  command => "sed -i \"42i\	add_header X-Served-By ${hostname};\" test_default",
+}
+service {'nginx':
+  ensure => 'running',
+  enable => true,
+}
