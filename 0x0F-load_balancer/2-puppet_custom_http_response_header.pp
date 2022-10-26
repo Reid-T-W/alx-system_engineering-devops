47183a692449c6a43fd50120cfed4b30b76cb7d5
Reid-T-W <rediettadesse100@gmail.com>
2022-10-26 05:13:59 +0300

    config file location changed 1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,16 +14,16 @@ file {'/usr/share/nginx/html/custom_404':
 }
 exec {'command_301':
   path    => '/bin',
-  command => 'sed -i "49i\	location \/redirect_me {\n		try_files \$uri =301;\n	}" /etc/nginx/sites-available',
+  command => 'sed -i "49i\	location \/redirect_me {\n		try_files \$uri =301;\n	}" /etc/nginx/sites-available/default',
 }
 exec {'command_404':
   path    => '/bin',
   command => 'sed -i "49i\	error_page 404 \/custom_404;\n	location = \/custom_404 \
-{\n		root \/usr\/share\/nginx\/html;\n		internal;\n	}" /etc/nginx/sites-available',
+{\n		root \/usr\/share\/nginx\/html;\n		internal;\n	}" /etc/nginx/sites-available/default',
 }
 exec {'command_header':
   path    => '/bin',
-  command => "sed -i \"42i\	add_header X-Served-By ${hostname};\" /etc/nginx/sites-available",
+  command => "sed -i \"42i\	add_header X-Served-By ${hostname};\" /etc/nginx/sites-available/default",
 }
 service {'nginx':
   ensure => 'running',
