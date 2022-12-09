17106c7a519306b6abc01b1706ea8ff210f8c534
Reid-T-W <rediettadesse100@gmail.com>
2022-12-09 13:17:46 +0000

    Hard and soft limits corrected for open files of Holberton user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# (Puppet file to configure open file limits on the Holberton user)
+exec {'sed_hard':
+  path    => '/bin',
+  command => 'sed -i "s/holberton hard nofile 5/#holberton hard nofile 5/g" /etc/security/limits.conf',
+}
+
+exec {'sed_soft':
+  path => '/bin',
+  command => 'sed -i "s/holberton soft nofile 4/#holberton soft nofile 4/g" /etc/security/limits.conf',
+}
